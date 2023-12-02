bca417352f29f2124beb1e09b02cdbc2bbdc528f
ghjattu <icracked369@gmail.com>
2023-12-02 15:33:12 +0800

    ✨ feat: calculate MD5 in chunks

--- client/src/utils/calculateMD5.js
+++ client/src/utils/calculateMD5.js
@@ -1,21 +1,44 @@
 import SparkMD5 from 'spark-md5';
+import GetChunkSize from './getChunkSize.js';
 
+// Calculate MD5 hash of file in chunks
 const CalculateMD5 = async (file) => {
 	return new Promise((resolve, reject) => {
+		const chunkSize = GetChunkSize();
+		const totalChunks = Math.ceil(file.size / chunkSize);
+		let currentChunk = 0;
+		const sparkArrayBuffer = new SparkMD5.ArrayBuffer();
 		const fileReader = new FileReader();
 
 		fileReader.onload = () => {
-			const result = fileReader.result;
-			const fileHash = SparkMD5.ArrayBuffer.hash(result);
+			console.log('currentChunk: ', currentChunk);
+			sparkArrayBuffer.append(fileReader.result);
+			currentChunk++;
 
-			resolve(fileHash);
+			if (currentChunk < totalChunks) {
+				loadNext();
+			} else {
+				console.log('finished loading file');
+				const fileHash = sparkArrayBuffer.end();
+				console.log('fileHash: ', fileHash);
+
+				resolve(fileHash);
+			}
+
 		};
 
 		fileReader.onerror = () => {
 			reject(new Error('File reading error'));
 		};
 
-		fileReader.readAsArrayBuffer(file);
+		const loadNext = () => {
+			const start = currentChunk * chunkSize;
+			const end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+
+			fileReader.readAsArrayBuffer(file.slice(start, end));
+		};
+
+		loadNext();
 	});
 };
 
