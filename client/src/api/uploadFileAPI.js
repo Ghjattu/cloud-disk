dbde3e94aacb8f1a9e978c3035c19c98f0fb67c4
ghjattu <icracked369@gmail.com>
2023-12-08 23:42:32 +0800

    🎈 perf:

--- client/src/api/uploadFileAPI.js
+++ client/src/api/uploadFileAPI.js
@@ -1,5 +1,3 @@
-import GetChunkSize from '../utils/getChunkSize.js';
-
 const CheckFileExistence = async (fileHash, token) => {
 	const url = `/file/exist/${fileHash}`;
 	const headers = {
@@ -16,7 +14,6 @@ const CheckFileExistence = async (fileHash, token) => {
 };
 
 const UploadFileInChunks = async (file, fileHash, chunksHash, uploadedChunksHash, token) => {
-	console.log('start uploading file in chunks');
 	const url = '/file/upload';
 	const headers = {
 		// add this line will cause error
@@ -25,14 +22,13 @@ const UploadFileInChunks = async (file, fileHash, chunksHash, uploadedChunksHash
 	};
 
 	return new Promise((resolve, reject) => {
-		const chunkSize = GetChunkSize(); // 100 KB
+		// eslint-disable-next-line no-undef
+		const windowSize = parseInt(process.env.REACT_APP_WINDOW_SIZE);
+		// eslint-disable-next-line no-undef
+		const chunkSize = parseInt(process.env.REACT_APP_CHUNK_SIZE);
 		const totalChunks = Math.ceil(file.size / chunkSize);
 
-		for (let chunkNum = 0; chunkNum < totalChunks; chunkNum++) {
-			if (uploadedChunksHash.includes(chunksHash[chunkNum])) {
-				continue;
-			}
-
+		const sendRequest = async (chunkNum) => {
 			const start = chunkNum * chunkSize;
 			const end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
 			const chunk = file.slice(start, end);
@@ -45,24 +41,33 @@ const UploadFileInChunks = async (file, fileHash, chunksHash, uploadedChunksHash
 				'file_hash': fileHash,
 				'total_chunks': totalChunks,
 				'chunk_hash': chunksHash[chunkNum],
-				'chunk_num': chunkNum
+				'chunk_num': chunkNum,
 			}));
 
-			fetch(url, {
-				method: 'POST',
-				headers: headers,
-				body: formData,
-			})
-				.then((response) => response.json())
-				.then((resp) => {
-					console.log('Upload chunk %d successfully', chunkNum);
-					if (resp.file_success) {
-						resolve(resp);
-					}
-				})
-				.catch((error) => {
-					reject(error);
+			try {
+				const response = await fetch(url, {
+					method: 'POST',
+					headers: headers,
+					body: formData,
 				});
+				const resp = await response.json();
+				if (resp.file_success) {
+					console.log('upload file success');
+					resolve(resp);
+				}
+				if (resp.chunk_success) {
+					console.log('upload chunk success!');
+					if (chunkNum + windowSize < totalChunks) {
+						sendRequest(chunkNum + windowSize);
+					}
+				}
+			} catch (err) {
+				reject(err);
+			}
+		};
+
+		for (let i = 0; i < Math.min(totalChunks, windowSize); i++) {
+			sendRequest(i);
 		}
 	});
 };
