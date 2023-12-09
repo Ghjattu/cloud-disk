e95b9aa34d53743eafa34511817811811d37e5dc
ghjattu <icracked369@gmail.com>
2023-12-09 20:02:44 +0800

    🦄 refactor:

--- services/user/api/internal/handler/loginhandler.go
+++ services/user/api/internal/handler/loginhandler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Ghjattu/cloud-disk/services/user/api/internal/svc"
 	"github.com/Ghjattu/cloud-disk/services/user/api/internal/types"
 	"github.com/zeromicro/go-zero/rest/httpx"
+	xhttp "github.com/zeromicro/x/http"
 )
 
 func LoginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
@@ -20,9 +21,9 @@ func LoginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 		l := logic.NewLoginLogic(r.Context(), svcCtx)
 		resp, err := l.Login(&req)
 		if err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			xhttp.JsonBaseResponseCtx(r.Context(), w, err)
 		} else {
-			httpx.OkJsonCtx(r.Context(), w, resp)
+			xhttp.JsonBaseResponseCtx(r.Context(), w, resp)
 		}
 	}
 }
