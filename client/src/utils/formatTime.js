45c54da57e4a9c1736de2bd33d8263537cb83e05
ghjattu <icracked369@gmail.com>
2023-12-09 21:12:53 +0800

    ✨ feat: add leading zero to minute

--- client/src/utils/formatTime.js
+++ client/src/utils/formatTime.js
@@ -13,20 +13,24 @@ const FormatTime = (time) => {
 	} else if (isYesterday) {
 		return `Yesterday ${format(date)}`;
 	} else {
-		// yy-mm-dd hh:mm
+		// yy-mm-dd
 		const year = date.getFullYear().toString().slice(-2);
 		const month = date.getMonth() + 1;
 		const day = date.getDate();
-		return `${year}-${month}-${day} ${format(date)}`;
+		return `${year}-${month}-${day}`;
 	}
 };
 
 // return hh:mm
 const format = (date) => {
 	const hours = date.getHours();
-	const minutes = date.getMinutes();
+	const minutes = padZero(date.getMinutes());
 
 	return `${hours}:${minutes}`;
+};
+
+const padZero = (num) => {
+	return num < 10 ? `0${num}` : num;
 };
 
 export default FormatTime;
